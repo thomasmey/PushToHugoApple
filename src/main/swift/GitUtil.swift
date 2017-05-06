a4c66e0883ae7906faaa4d6794dde44854ddc92a
Thomas Meyer <thomas@m3y3r.de>
2017-05-06 12:03:42 +0200

    Some more Swift experiments

--- src/main/swift/GitUtil.swift
+++ src/main/swift/GitUtil.swift
@@ -15,4 +15,49 @@ func createBlogPost(title: String, url: String, date: Date, extras: [String:Stri
 	return bp
 }
 
+func createRepoAndAddPost(title: String, link: String, extras: [String:String]) {
+
+	let currentDate = Date()
+	let repoUrl = URL(string: getRemoteUrl())
+
+	let blogPost = [UInt8] (createBlogPost(title: title, url: link, date: currentDate, extras: extras).utf8)
+	
+	let (gitBlobObjectId, gitBlobData) = gitCreateObject(type: "blob", data: blogPost);
+
+}
+
+func getRemoteUrl() -> String {
+
+	return "GET http://server/simplegit-progit.git/info/refs?service=git-receive-pack";
+}
+
+/* creates an git object,
+ * see https://www.kernel.org/pub/software/scm/git/docs/user-manual.html#object-details
+ */
+func gitCreateObject(type: String, data: [UInt8]) -> ([UInt8], [UInt8]) {
+
+	let objectWithHeader = gitObjectAddHeader(type: type, data: data)
+	let objectId = determineObjectId(data: objectWithHeader);
+
+	// compress data
+	let compressedObject = compressObject(data: objectWithHeader)
+	return (objectId, compressedObject)
+}
+
+func determineObjectId(data: [UInt8]) -> [UInt8] {
+	// returns the SHA-1 digest of the input data
+	let sha1 = [UInt8] (repeating: 0, count:12)
+	return sha1
+}
+
+func gitObjectAddHeader(type: String, data: [UInt8]) -> [UInt8] {
+	let header = "\(type) \(data.count)\0"
+
+	// insert headerbefore data
+	return [UInt8](header.utf8) + data
+}
+
+func compressObject(data: [UInt8]) -> [UInt8] {
+	return data;
+}
 
